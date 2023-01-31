bb2f78196139a5182f4e9540fd44eafa783818bf
morifumiya <morimorimorifu@gmail.com>
2023-01-31 13:12:24 +0900

    begin : tada プロジェクトの開始 commit test

---
+++ app/src/main/java/com/test/eggsnaky/MainActivity.java
@@ -0,0 +1,19 @@
+package com.test.eggsnaky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textViewOfBrilliandroid);
+        textView.setBackgroundColor(Color.DKGRAY);
+    }
+}
